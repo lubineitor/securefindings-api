7ff19845de95306522892b6130aed279c8be77bc
Lubineitor777 <rubenpol6@gmail.com>
2026-08-31 06:02:48 +0200

    estructura: crear base inicial de Spring Boot

---
+++ src/main/java/com/securefindings/SecureFindingsApplication.java
@@ -0,0 +1,12 @@
+package com.securefindings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFindingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureFindingsApplication.class, args);
+    }
+}
